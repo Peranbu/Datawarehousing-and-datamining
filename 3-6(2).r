a9440ef68dc58149e02ad412fd942643e74a4f6a
Peranbu <112739680+Peranbu@users.noreply.github.com>
2023-11-23 22:28:00 +0530

    Create 3-6(2).r

---
+++ 3-6(2).r
@@ -0,0 +1,33 @@
+# Define the scores
+classA <- c(76, 35, 47, 64, 95, 66, 89, 36, 84, 76, 35, 47, 64, 95, 66, 89, 36, 84)
+classB <- c(51, 56, 84, 60, 59, 70, 63, 66, 50, 51, 56, 84, 60, 59, 70, 63, 66, 50)
+# Calculate the mean
+meanA <- mean(classA)
+meanB <- mean(classB)
+# Calculate the median
+medianA <- median(classA)
+medianB <- median(classB)
+# Calculate the range
+rangeA <- max(classA) - min(classA)
+rangeB <- max(classB) - min(classB)
+# Print the results
+print(paste("Mean of Class A: ", meanA))
+print(paste("Mean of Class B: ", meanB))
+print(paste("Median of Class A: ", medianA))
+print(paste("Median of Class B: ", medianB))
+print(paste("Range of Class A: ", rangeA))
+print(paste("Range of Class B: ", rangeB))
+# Plot the boxplot
+boxplot(classA, classB, names=c("Class A", "Class B"), main="Boxplot of Scores")
+
+
+# Define the data
+data <- c(200, 300, 400, 600, 1000)
+# Min-max normalization
+min_max_normalized <- (data - min(data)) / (max(data) - min(data))
+print(paste("Min-max normalized data: ", toString(min_max_normalized)))
+# Z-score normalization
+z_score_normalized <- (data - mean(data)) / sd(data)
+print(paste("Z-score normalized data: ", toString(z_score_normalized)))
+
+
